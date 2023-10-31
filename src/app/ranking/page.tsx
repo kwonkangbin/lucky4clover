e0a6bc33dd3eb3e720a77d3e800ab53e658a5cb3
daveg7lee <daveg7lee@gmail.com>
2023-10-31 23:52:55 +0900

    ✨ 랭킹 페이지

--- src/app/ranking/page.tsx
+++ src/app/ranking/page.tsx
@@ -1,41 +1,49 @@
 "use client";
 
-import Button from "@/components/atoms/Button";
 import {
   BackIcon,
   HomeIcon,
   PostIcon,
   RankingIcon,
+  SearchIcon,
   ShareIcon,
 } from "@/components/icon";
 import Link from "next/link";
 
-const Home = () => {
+const Ranking = () => {
   return (
     <div className="max-w-md mx-auto px-6 flex flex-col items-center relative">
-      <div className="flex justify-between pt-[30px] pb-9 w-full">
-        <BackIcon />
-        <ShareIcon />
+      <div className="flex justify-between items-center pt-[30px] pb-[16px] w-full">
+        <div className="flex items-center">
+          <BackIcon />
+          <p className="ml-[15px] text-[20px] font-semibold text-black-2">
+            랭킹
+          </p>
+        </div>
+        <SearchIcon />
       </div>
-      <h1 className="font-semibold text-[26px] leading-[37px] text-left w-full">
-        000명의 친구들이 <br /> Name님을 <br /> 응원하고 있어요!
-      </h1>
-      <img
-        src="/img/Home.png"
-        alt="four leaf clover"
-        className="mx-auto mt-[32px]"
-      />
-      <h2 className="text-[27px] font-semibold text-center mb-[23px]">00등</h2>
-      <p className="text-[14px] font-semibold text-center mb-[13px]">
-        인스타그램 스토리에 언급하고 응원글을 더 받아보세요
-      </p>
-      <Button varient="instargram">인스타그램 스토리에 언급하기</Button>
+      <div className="bg-green-1 p-[15px] w-full pb-[22px] rounded-[20px]">
+        <p className="text-[14px] font-bold text-white mb-[12px]">
+          NAME님의 랭킹
+        </p>
+        <div className="flex items-center">
+          <div className="p-[13px] bg-white rounded-full w-[62px] h-[62px] mr-[11px]">
+            <img src="/img/Ranking.png" alt="four leaf" />
+          </div>
+          <div className="flex flex-col items-start">
+            <p className="mb-[5px] text-white text-[14px] font-bold">00등</p>
+            <p className="text-white text-[14px] font-bold">
+              받은 응원글: 14개
+            </p>
+          </div>
+        </div>
+      </div>
       <div className="fixed bottom-0 max-w-md px-6 w-full">
         <div className="bg-green-1 flex w-full pt-[14px] px-[50px] pb-[14px] rounded-t-[12px] justify-start">
           <div>
             <Link href="/ranking">
               <div className="flex flex-col items-center justify-center gap-1 cursor-pointer">
-                <RankingIcon select={false} />
+                <RankingIcon select />
                 <p className="text-[14px] font-semibold text-white text-center">
                   랭킹
                 </p>
@@ -45,8 +53,8 @@ const Home = () => {
           <div className="mx-auto">
             <Link href="/home">
               <div className="flex flex-col items-center justify-center  gap-1 cursor-pointer">
-                <HomeIcon select />
-                <p className="text-[14px] font-semibold text-white text-center">
+                <HomeIcon select={false} />
+                <p className="text-[14px] font-semibold text-white text-center text-opacity-70">
                   홈
                 </p>
               </div>
@@ -56,7 +64,7 @@ const Home = () => {
             <Link href="/messages">
               <div className="flex flex-col items-center justify-center gap-1 cursor-pointer">
                 <PostIcon select={false} />
-                <p className="text-[14px] font-semibold text-white text-center">
+                <p className="text-[14px] font-semibold text-white text-center text-opacity-70">
                   응원글
                 </p>
               </div>
@@ -68,4 +76,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default Ranking;
