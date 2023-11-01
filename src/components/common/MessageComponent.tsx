45baa9014b711626c946ca0445f8086f46c401e1
daveg7lee <daveg7lee@gmail.com>
2023-11-01 11:08:34 +0900

    ✨ 응원글 페이지

---
+++ src/components/common/MessageComponent.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+interface IMessageComponentProps {
+  name: string;
+  message: string;
+}
+
+const MessageComponent = ({ name, message }: IMessageComponentProps) => {
+  return (
+    <div className="flex px-[17px] py-[20px] items-center justify-between w-full shadow-[0_4px_4px_0px_rgba(0,0,0,0.11)] mb-[12px] rounded-[12px]">
+      <div className="flex items-center">
+        <div className="p-[8px] bg-black-2 rounded-full w-[36px] h-[36px] mr-[11px]">
+          <Image
+            src="/img/Ranking.png"
+            alt="four leaf"
+            width={20}
+            height={20}
+          />
+        </div>
+        <h2 className="text-black-1 text-[16px] font-bold">{name}</h2>
+      </div>
+      <p className="text-black-3 text-[14px] font-medium overflow-hidden text-ellipsis whitespace-nowrap w-[184px]">
+        {message}
+      </p>
+    </div>
+  );
+};
+
+export default MessageComponent;
